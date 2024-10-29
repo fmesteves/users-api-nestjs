a8c38ebaf41d4b6b9501d3ba48430bbfcdeb3dce
Fabio Esteves <fabioesteves@MacBook-Pro-de-Fabio.local>
2024-10-28 22:28:00 -0300

    Remove sqlite

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'John Doe' })
+  @ApiProperty({ example: 'João da Silva' })
   @IsString()
   name: string;
 
-  @ApiProperty({ example: 'john.doe@example.com' })
+  @ApiProperty({ example: 'joao@exemplo.com' })
   @IsEmail()
   email: string;
 }
